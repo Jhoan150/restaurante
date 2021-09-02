fe0c68fdaa544b83e62c7a74a149a4bae0b643a0
Jhoan150 <echavarriajhoan14@gmail.com>
2021-09-02 11:18:50 -0500

    commit

---
+++ app/Http/Controllers/PruebaController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\comida;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
+
+class PruebaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $comidas = comida::all();
+        return $comidas;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request)
+    {
+       
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $comidas = new comida;
+        $comidas->name = $request->name;
+        $comidas->save();
+
+        return 'la comida fue registrada';
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $comida = comida::find($id);
+
+        return json_encode($comida);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $comida = comida::find($id);
+        $comida->name = $request->name;
+        $comida->save();
+        return json_encode($comida);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $comida = comida::find($id);
+        $comida->delete();
+        return 'la comida fue eliminada';
+    }
+}
